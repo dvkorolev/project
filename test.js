b12ef1b5743877aa21bd9037f96892dcb10aff93
d.korolyov <d.korolyov@gmail.com>
2020-04-12 18:10:37 +0300

    first

---
+++ test.js
@@ -0,0 +1,20 @@
+/*
+function fourLetters(names) {
+    let countOfNames = 0;
+    slicedNames = names.split(" ");   
+    for (let name of slicedNames) {
+        if (name.length == 4) 
+        countOfNames ++;
+    }
+    return countOfNames;
+     
+}
+
+console.log(fourLetters("Tror Gvigris Deriana Nori"));*/
+
+/**
+ * Реализуйте функцию getTotalTime
+ */
+function getTotalTime(heroes, n) {
+    
+}
