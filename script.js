1d58c63834ea66e560b7d265bbdd7ac52ead44b3
dmitry_kor <d.korolyov@gmailc.om>
2020-04-13 19:48:36 +0300

    first homework

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-function createParagraph() {
-    let para = document.createElement('p');
-    para.textContent = 'You clicked the button!';
-    document.body.appendChild(para);
-  }
-  
-  const buttons = document.querySelectorAll('button');
-  
-  for(let i = 0; i < buttons.length ; i++) {
-    buttons[i].addEventListener('click', createParagraph);
-  }
+"user strict"; 
+
+let money = prompt("Ваш бюджет на месяц?",""),
+      time = prompt("Введите дату в формате YYYY-MM-DD","");
+
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+let a1 = prompt('Введите обязательную статью расходов в этом месяце',""),
+      a2 = prompt("Во сколько обойдется?",""),
+      a3 = prompt('Введите обязательную статью расходов в этом месяце',""),
+      a4 = prompt("Во сколько обойдется?","");
+
+appData.expenses[a1] = a2;
+appData.expenses[a2] = a4;
+
+alert(appData.budget / 30);
